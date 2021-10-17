3553be8b673bed74bcc2737c476326f303cd70e9
LeeMin-hyeong <lmhstrar070@naver.com>
2021-10-17 20:25:22 +0900

    practice

---
+++ practice/pr10.c
@@ -0,0 +1,5 @@
+#include <stdio.h> 
+int main(int argc, char *argv[]){
+    printf("%d", *argv[2]-*argv[4]);
+    return 0;
+} 
--- practice/pr3.c
+++ practice/pr3.c
@@ -13,7 +13,7 @@ int main(){
     stu1.name=(char *)malloc(strlen("lee")+1);
     strcpy(stu1.name, "lee");
     printf("%s\n", stu1.name);
-    if(!strcmp(stu1.name, stu2.name))
+    if(stu1.name==stu2.name)
         printf("두 구조체는 같습니다.");
     else
         printf("두 구조체는 다릅니다.");

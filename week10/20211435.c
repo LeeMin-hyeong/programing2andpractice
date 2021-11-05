b85af8808432180b9b16534002c9a269e2bb1d2f
leemin-hyeong <lmhstrar070@naver.com>
2021-11-05 19:03:29 +0900

    211103

--- week10/20211435.c
+++ week10/20211435.c
@@ -21,82 +21,79 @@ int getch(void) {
 void print_puzzle(int a[]){
     system("clear");
     for(int i=0; i<16; i++){
-        if(i%4==0) printf("\t\t\t");
-        if(a[i]==0) printf("   ");
-        else printf("%3d", a[i]);
-        if(i%4==3) printf("\n");
+        if(i%4==0)
+            printf("\t\t\t");
+        if(a[i]==0)
+            printf("   ");
+        else
+            printf("%3d", a[i]);
+        if(i%4==3)
+            printf("\n");
     }
 }
 
-void random_puzzle(int a[]){
-    srand(time(NULL));
-    for(int i=0; i<16; i++){
-        a[i]=rand()%16;
-        for(int j=0; j<i; j++){
-            if(a[i]==a[j]){
-                i--;
-                break;
-            }
-        }
-    }
-}
-
-int check_puzzle(int a[]){
-    int count=0;
-    for(int i=0; i<15; i++)
-        if(a[i]==i+1) count++;
-    if(count==15) return 1;
-    else return 0;
-}
-
-int movement(int a[]){
-    int ch, i, j=0;
+int movement(int a[], int b){
+    int ch, i, j=1;
     for(i=0; i<16; i++)
-        if(a[i]==0) break;
-    ch=getch();
+        if(a[i]==0)
+            break;
 
-    if(ch=='j'&&i%4!=0){
+    if(b=='j'&&i%4!=0){
         a[i]=a[i-1];
         a[i-1]=0;
     }
-    else if(ch=='k'&&i<12){
+    else if(b=='k'&&i<12){
         a[i]=a[i+4];
         a[i+4]=0;
     }
-    else if(ch=='l'&&i%4!=3){
+    else if(b=='l'&&i%4!=3){
         a[i]=a[i+1];
         a[i+1]=0;
     }
-    else if(ch=='i'&&i>3){
+    else if(b=='i'&&i>3){
         a[i]=a[i-4];
         a[i-4]=0;
     }
-    else if(ch=='q')
-        j=1;
+    else if(b=='q')
+        j=0;
     else
         j=2;
     
     return j;
 }
 
+void random_puzzle(int a[]){
+    srand(time(NULL));
+    for(int i=0; i<rand(); i++)
+        movement(a, rand()%4+'i');
+}
+
+int check_puzzle(int a[]){
+    int count=0;
+    for(int i=0; i<15; i++)
+        if(a[i]==i+1)
+            count++;
+    if(count==15)
+        return 1;
+    else
+        return 0;
+}
+
 int main(int argc, char *argv[]){
-    int puzzle[16], count=0;
+    int count=0, puzzle[16]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
     system("clear");
-    if(atoi(argv[1])==1){
-        for(int i=0; i<15; i++)
-            puzzle[i]=i+1;
-        puzzle[15]=0;
-    }
-    else if(atoi(argv[1])==2)
+    if(atoi(argv[1])==2)
         random_puzzle(puzzle);
-    else return 0;
-    
+    else if(atoi(argv[1])!=1)
+        return 0;
+
     while(1){
         print_puzzle(puzzle);
-        int x=movement(puzzle);
-        if(x==1) break;
-        else if(x==2) continue;
-        else count++;
+        int x=movement(puzzle, getch());
+        if(x==1)
+            count++;
+        else if(x==0)
+            break;
 
         if(check_puzzle(puzzle)&&count>0){
             print_puzzle(puzzle);
